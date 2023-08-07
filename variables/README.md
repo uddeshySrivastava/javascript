616600604a6cde29f3136ada16d6907213a2adc7
uddeshySrivastava <135315891+uddeshySrivastava@users.noreply.github.com>
2023-08-07 18:50:35 +0000

    variables

---
+++ variables/README.md
@@ -0,0 +1,29 @@
+# Variables
+
+- 5 vedios on youtube daily
+ - https://www.youtube.com/watch?v=Q4p8vRQX8uY&pp=ygUUamF2YXNjcmlwdCB2YXJpYWJsZSA%3D
+ -https://www.youtube.com/watch?v=--Vh17ocC_s&pp=ygUUamF2YXNjcmlwdCB2YXJpYWJsZSA%3D
+ -https://www.youtube.com/watch?v=XIOLqoPHCJ4&pp=ygUiamF2YXNjcmlwdCB2YXJpYWJsZSBjb2RlIGV2b2x1dGlvbg%3D%3D
+ -https://www.youtube.com/watch?v=yY0bKZNYmJs&pp=ygUiamF2YXNjcmlwdCB2YXJpYWJsZSBjb2RlIGV2b2x1dGlvbg%3D%3D
+ -https://www.youtube.com/watch?v=DHjqpvDnNGE&pp=ygUiamF2YXNjcmlwdCB2YXJpYWJsZSBjb2RlIGV2b2x1dGlvbg%3D%3D
+ -https://www.youtube.com/watch?v=9WIJQDvt4Us&pp=ygUeamF2YXNjcmlwdCB2YXJpYWJsZSBzaW1wbGlmaWVk
+
+
+ # Vedio 1 
+ ## variables 
+ - variables is a container that stores value.
+ - the value of a javascript variables can be change during the exicution of a Program.
+ ## difference between Let , Var , Const.
+ -  var are globally scoped but let and const are blocked scoped.
+ - var can be update and re-declared with in the scope.
+ - let can be update but cann't re-declared.
+ - const can neither be update nor be re-decleared.
+ - const must be initlized during decleration  
+
+ # Javascript 
+ - scripting language
+ - dynamically type
+ -  case sensetive language
+ -  non blocking language ( put process on wating )
+ -  Single threaded       ()
+ - Interpreter language   (run line wise)
--- variables/index.js
+++ variables/index.js
@@ -1,2 +1,83 @@
-const array = "hello"
-console.log(array)
+// var person = "uddesh"
+// person = "yash"
+
+// console.log(person);
+
+
+// block scope 
+
+let number = 56
+// number = 85
+
+// console.log(number);
+
+
+let b = "akash"
+
+{
+    let b = "yash"
+    const c= number
+    console.log(b);
+}
+
+// console.log(b)  
+   
+// const harry = 25;
+// harry = 35  //conn't be change b,coz its constant. will show errer
+// console.log(harry);     
+
+// function scope block scope
+
+function modjs(animal) {
+    if (animal) {
+        let bird = "parrot"
+        const dog = "pamerion"
+        console.log("the bird is "+ bird +"the dog is "+dog);
+    }
+}
+modjs(true)
+
+
+function modjs(animal) {
+    if (animal) {
+        var bird = "parrot"
+        var dog = "pamerion"
+    }
+    console.log("the bird is "+ bird +"the dog is "+dog);
+}
+modjs(true)
+
+// variable can store data and transfer data 
+
+let alpha = 1
+ alpha = 2
+ alpha = 3
+ alpha = "4"
+ alpha = "zero"
+ console.log(alpha);
+
+
+ let person = "akash"
+ let car = "maruti"
+ let mobile = alpha 
+ mobile  = alpha
+ alpha = mobile
+ let table = "wooden"
+console.log(alpha);
+
+
+let num = 56
+num =85
+console.log(num);
+
+// read only memory
+const legal = false
+const entry = legal
+console.log(entry);
+
+const obj = {
+    fname : "abhay",
+    lname : "soni"
+}
+
+obj.fname = "yash"
